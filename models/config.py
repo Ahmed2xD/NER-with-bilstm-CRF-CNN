1bea562dfaf1780cf7a30897846df25b06c2c59e
Ahmed samir <ahmed.tolan97@eng-st.cu.edu.eg>
2019-09-09 20:48:52 +0200

    Update config.py

--- models/config.py
+++ models/config.py
@@ -1,12 +1,11 @@
-# 设置lstm训练参数
+# Set lstm training parameters
 class TrainingConfig(object):
     batch_size = 64
-    # 学习速率
     lr = 0.001
     epoches = 30
     print_step = 5
 
 
 class LSTMConfig(object):
-    emb_size = 128  # 词向量的维数
-    hidden_size = 128  # lstm隐向量的维数
+    emb_size = 128  
+    hidden_size = 128 
