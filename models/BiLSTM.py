bb00f41075c51b73c43596217135b3898ac0a0ee
Ahmed samir <ahmed.tolan97@eng-st.cu.edu.eg>
2019-09-09 20:43:41 +0200

    Update BiLSTM.py

--- models/BiLSTM.py
+++ models/BiLSTM.py
@@ -1,9 +1,3 @@
-# @Author : bamtercelboo
-# @Datetime : 2018/8/17 16:06
-# @File : BiLSTM.py
-# @Last Modify Time : 2018/8/17 16:06
-# @Contact : bamtercelboo@{gmail.com, 163.com}
-
 """
     FILE :  BiLSTM.py
     FUNCTION : None
@@ -74,11 +68,11 @@ class BiLSTM(nn.Module):
 
 class BiLSTM(nn.Module):
     def __init__(self, vocab_size, emb_size, hidden_size, out_size):
-        """初始化参数：
-            vocab_size:字典的大小
-            emb_size:词向量的维数
-            hidden_size：隐向量的维数
-            out_size:标注的种类
+        """：
+            vocab_size:
+            emb_size:
+            hidden_size：
+            out_size:
         """
         super(BiLSTM, self).__init__()
         self.embedding = nn.Embedding(vocab_size, emb_size)
@@ -101,7 +95,6 @@ class BiLSTM(nn.Module):
         return scores
 
     def test(self, sents_tensor, lengths, _):
-        """第三个参数不会用到，加它是为了与BiLSTM_CRF保持同样的接口"""
         logits = self.forward(sents_tensor, lengths)  # [B, L, out_size]
         _, batch_tagids = torch.max(logits, dim=2)
 
