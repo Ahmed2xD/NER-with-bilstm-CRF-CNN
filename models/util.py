32ee5775e78b78117c6bec5501d25f55ef74f001
Ahmed samir <ahmed.tolan97@eng-st.cu.edu.eg>
2019-09-09 22:03:21 +0200

    Update util.py

--- models/util.py
+++ models/util.py
@@ -1,17 +1,14 @@
 import torch
 import torch.nn.functional as F
 
-# ******** CRF 工具函数*************
+# ******** CRF tool function*************
 
 
 def word2features(sent, i):
-    """抽取单个字的特征"""
+    """Extract the characteristics of a single word"""
     word = sent[i]
     prev_word = "<s>" if i == 0 else sent[i-1]
     next_word = "</s>" if i == (len(sent)-1) else sent[i+1]
-    # 使用的特征：
-    # 前一个词，当前词，后一个词，
-    # 前一个词+当前词， 当前词+后一个词
     features = {
         'w': word,
         'w-1': prev_word,
@@ -24,7 +21,6 @@ def word2features(sent, i):
 
 
 def sent2features(sent):
-    """抽取序列特征"""
     return [word2features(sent, i) for i in range(len(sent))]
 
 
@@ -41,7 +37,6 @@ def tensorized(batch, maps):
     for i, l in enumerate(batch):
         for j, e in enumerate(l):
             batch_tensor[i][j] = maps.get(e, UNK)
-    # batch各个元素的长度
     lengths = [len(l) for l in batch]
 
     return batch_tensor, lengths
@@ -61,12 +56,6 @@ def sort_by_lengths(word_lists, tag_lists):
 
 
 def cal_loss(logits, targets, tag2id):
-    """计算损失
-    参数:
-        logits: [B, L, out_size]
-        targets: [B, L]
-        lengths: [B]
-    """
     PAD = tag2id.get('<pad>')
     assert PAD is not None
 
@@ -86,9 +75,6 @@ def cal_loss(logits, targets, tag2id):
 
 
 def cal_lstm_crf_loss(crf_scores, targets, tag2id):
-    """计算双向LSTM-CRF模型的损失
-    该损失函数的计算可以参考:https://arxiv.org/pdf/1603.01360.pdf
-    """
     pad_id = tag2id.get('<pad>')
     start_id = tag2id.get('<start>')
     end_id = tag2id.get('<end>')
@@ -116,30 +102,14 @@ def cal_lstm_crf_loss(crf_scores, targets, tag2id):
     golden_scores = flatten_scores.gather(
         dim=1, index=targets.unsqueeze(1)).sum()
 
-    # 计算golden_scores方法２：利用pack_padded_sequence函数
-    # targets[targets == end_id] = pad_id
-    # scores_at_targets = torch.gather(
-    #     crf_scores.view(batch_size, max_len, -1), 2, targets.unsqueeze(2)).squeeze(2)
-    # scores_at_targets, _ = pack_padded_sequence(
-    #     scores_at_targets, lengths-1, batch_first=True
-    # )
-    # golden_scores = scores_at_targets.sum()
-
-    # 计算all path scores
-    # scores_upto_t[i, j]表示第i个句子的第t个词被标注为j标记的所有t时刻事前的所有子路径的分数之和
     scores_upto_t = torch.zeros(batch_size, target_size).to(device)
     for t in range(max_len):
-        # 当前时刻 有效的batch_size（因为有些序列比较短)
         batch_size_t = (lengths > t).sum().item()
         if t == 0:
             scores_upto_t[:batch_size_t] = crf_scores[:batch_size_t,
                                                       t, start_id, :]
         else:
-            # We add scores at current timestep to scores accumulated up to previous
-            # timestep, and log-sum-exp Remember, the cur_tag of the previous
-            # timestep is the prev_tag of this timestep
-            # So, broadcast prev. timestep's cur_tag scores
-            # along cur. timestep's cur_tag dimension
+           
             scores_upto_t[:batch_size_t] = torch.logsumexp(
                 crf_scores[:batch_size_t, t, :, :] +
                 scores_upto_t[:batch_size_t].unsqueeze(2),
@@ -147,13 +117,11 @@ def cal_lstm_crf_loss(crf_scores, targets, tag2id):
             )
     all_path_scores = scores_upto_t[:, end_id].sum()
 
-    # 训练大约两个epoch loss变成负数，从数学的角度上来说，loss = -logP
     loss = (all_path_scores - golden_scores) / batch_size
     return loss
 
 
 def indexed(targets, tagset_size, start_id):
-    """将targets中的数转化为在[T*T]大小序列中的索引,T是标注的种类"""
     batch_size, max_len = targets.size()
     for col in range(max_len-1, 0, -1):
         targets[:, col] += (targets[:, col-1] * tagset_size)
